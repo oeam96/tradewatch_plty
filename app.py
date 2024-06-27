39d2b93301e1a4802ccf13fc73f8f7fac1467564
oeam96 <oscardg96@gmail.com>
2024-06-27 11:21:28 +0200

    Updating the parent container

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-#THIS APPLICATION IS BASED ON THE ONE PUBLISHED BY TANYA LOMSKAYA WHICH IS PART OF THE "DASH EXAMPLE GALLERY"
+#THIS APPLICATION IS BASED ON THE ONE PUBLISHED BY TANYA LOMSKAYA
+#WHICH IS PART OF THE "DASH EXAMPLE GALLERY"
 #Link will be provided after
 
 from dash import Dash, html, dcc
@@ -14,9 +15,43 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.css.config.serve_locally = True
 
 #Main Layout
-app.layout = dbc.Container([html.Div(), html.Div()],
-                            fluid=True,
-                            className="dashboard-container")
+app.layout = dbc.Container([
+        html.Div([
+                html.Div([
+                    html.H1([
+                        html.Span("Welcome"),
+                        html.Span("to the Dashboard Demo")
+                    ]),
+                    html.P("This is a demo in construction that will display trade data")
+                ],
+                    style={
+                        "vertical-alignment": "top",
+                        "height": 260
+                    }
+                ),
+                html.Div(),
+                html.Div(),
+                html.Div()
+        ],
+        style={
+        'width': 340,
+        'margin-left': 35,
+        'margin-top': 35,
+        'margin-bottom': 35,
+    }),
+    html.Div(
+        [html.Div(style={'width': 790}),
+         html.Div(style={'width': 200})],
+        style={
+        'width': 990,
+        'margin-top': 35,
+        'margin-right': 35,
+        'margin-bottom': 35,
+        'display': 'flex'
+    })
+],
+    fluid=True,
+    className="dashboard-container")
 
 #Runnin the app with development server in debug mode
 if __name__ == "__main__":
